5834c2de366595c54b7b75290e81b5718ef8f660
hahuaz <hahuaz@gmail.com>
2021-08-23 17:23:56 +0300

    add product

---
+++ components/product.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div
+      class="
+        product__container
+        tw-max-w-xs
+        tw-bg-white
+        tw-text-center
+        tw-space-y-4
+        tw-p-4
+        tw-border
+        tw-border-gray-200
+      "
+    >
+      <div class="product__image tw-w-48 tw-h-60 tw-mx-auto">
+        <img
+          class="tw-w-full tw-h-full tw-object-cover tw-object-center"
+          :src="product.image"
+          alt="image for product"
+        />
+      </div>
+      <div class="product__content tw-space-y-4">
+        <p class="product__title tw-text-gray-500">{{ product.title }}</p>
+        <div class="tw-flex tw-items-center">
+          <span
+            class="
+              product__price
+              tw-font-semibold tw-text-gray-600 tw-flex-grow
+            "
+          >
+            {{ product.price }} &#8378;
+          </span>
+          <button
+            class="
+              product__add
+              tw-py-2
+              tw-px-4
+              tw-bg-primary
+              tw-font-semibold
+              tw-text-white
+              tw-rounded
+              tw-flex-shrink
+              tw-text-sm
+            "
+          >
+            ADD BASKET
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  data() {
+    return {}
+  },
+  computed: {
+    currencyEntity() {
+      return this.$store.state.ui.currencyEntity
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,86 @@
 <template>
-  <div class="tw-bg-red-500">hi from index</div>
+  <div class="">
+    <div class="tw-max-w-screen-xl tw-mx-auto">
+      <div class="tw-container tw-mx-auto tw-p-4"></div>
+      <div class="products tw-flex tw-flex-wrap tw-justify-center tw-px-6">
+        <product
+          v-for="(product, i) in filteredProducts"
+          :key="i"
+          :product="product"
+        ></product>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      products: [
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 130,
+          image:
+            'https://images.unsplash.com/photo-1556228578-0d85b1a4d571?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+          stock: 15,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 130,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 0,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+        {
+          title: 'Lorem Ipsun Doler Siteats Amete Caste ',
+          price: 120,
+          image:
+            'https://images.unsplash.com/photo-1564466809058-bf4114d55352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80',
+          stock: 2,
+        },
+      ],
+    }
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter((product) => product.stock !== 0)
+    },
+  },
+}
 </script>
